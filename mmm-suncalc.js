e50fb0b69785872e0c1ea2ca11f96eb9a9b6f045
Ketil Skjerve <ketil.skjerve@gmail.com>
2021-01-02 16:28:16 +0100

    Replaced moment.js with builtin DateTimeFormat

--- mmm-suncalc.js
+++ mmm-suncalc.js
@@ -26,42 +26,39 @@
 */
 
 Module.register("mmm-suncalc",{
-  times: null,
-  // Default module config.
-  defaults: {
-    latitude: 60.794533,
-    longitude: 11.067998,
-    updateInterval: 1 * 60 * 60 * 1000, // every 1 hour
-    text: "The sun rises at <sunrise>, and sets at <sunset>",
-  },
-  start: function() {
-    Log.info("Starting module: " + this.name);
-    this.sendSocketNotification('SET_CONFIG', this.config);
-    moment.locale(config.language);
-    setInterval( function(){this.doCalculate();} , this.config.updateInterval);
-  },
-  getScripts: function(){
-    return [ 'moment.js', ];
-  },
-  socketNotificationReceived: function(notification, payload) {
-    if(notification == 'SUNCALC_CALC'){
-      this.times = payload;
-      this.updateDom();
-    }
-  },
-  // Override dom generator.
-  getDom: function() {
-    var wrapper = document.createElement("div");
-    wrapper.className = this.config.classes ? this.config.classes : "small";
-    if( this.times != null ){
-      var res = this.config.text;
-      for ( var time in this.times ){
-        res = res.replace("<"+time+">", new moment(this.times[time]).format("LT"));
-      }
-      wrapper.innerHTML = res;
-    } else {
-      wrapper.innerHTML = "";
-    }
-    return wrapper;
-  }
+	times: null,
+	// Default module config.
+	defaults: {
+		latitude: 60.794533,
+		longitude: 11.067998,
+		updateInterval: 1 * 60 * 60 * 1000, // every 1 hour
+		text: "The sun rises at <sunrise>, and sets at <sunset>",
+	},
+	start: function() {
+		Log.info("Starting module: " + this.name);
+		this.sendSocketNotification('SET_CONFIG', this.config);
+		setInterval( function(){this.doCalculate();} , this.config.updateInterval);
+	},
+	socketNotificationReceived: function(notification, payload) {
+		if(notification == 'SUNCALC_CALC'){
+			this.times = payload;
+			this.updateDom();
+		}
+	},
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		wrapper.className = this.config.classes ? this.config.classes : "small";
+		if( this.times != null ){
+			var res = this.config.text;
+			for ( var time in this.times ){
+				var t = Intl.DateTimeFormat(config.language, { hour: "numeric", minute: "numeric" }).format(new Date(this.times[time]))
+                res = res.replace("<"+time+">", t);
+			}
+			wrapper.innerHTML = res;
+		} else {
+			wrapper.innerHTML = "";
+		}
+		return wrapper;
+	}
 });
